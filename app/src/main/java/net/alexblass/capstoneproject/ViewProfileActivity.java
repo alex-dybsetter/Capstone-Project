8c18301c4c22f5ce11b100505caa8f703f2e92aa
Alex Blass <ajblack.000@hotmail.com>
2017-12-17 01:07:51 -0800

    Feat: Add Firebase Admob to display in ViewProfile

--- app/src/main/java/net/alexblass/capstoneproject/ViewProfileActivity.java
+++ app/src/main/java/net/alexblass/capstoneproject/ViewProfileActivity.java
@@ -3,11 +3,23 @@ package net.alexblass.capstoneproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ViewProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.view_profile_ad) AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+        ButterKnife.bind(this);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
